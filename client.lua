ca5f0af52f4d2c8b9568c634fdf1901416e9d904
ngomez93 <82259641+ngomez93@users.noreply.github.com>
2025-10-03 13:12:50 -0700

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,9 +1,11 @@
--- Send duty status to server periodically
-CreateThread(function()
-    while true do
-        local isLEO = exports['SEM_InteractionMenu']:IsOndutyLEO()
-        local isFire = exports['SEM_InteractionMenu']:IsOndutyFire()
-        TriggerServerEvent("dutySync:updateStatus", isLEO, isFire)
-        Wait(5000) -- check every 5 seconds
-    end
-end)
+if Config.Framework == "SEM" then
+    -- SEM uses client exports, so clients must report status
+    CreateThread(function()
+        while true do
+            local isLEO = exports['SEM_InteractionMenu']:IsOndutyLEO()
+            local isFire = exports['SEM_InteractionMenu']:IsOndutyFire()
+            TriggerServerEvent("dutySync:updateStatus", isLEO, isFire)
+            Wait(5000) -- send every 5 seconds
+        end
+    end)
+end
